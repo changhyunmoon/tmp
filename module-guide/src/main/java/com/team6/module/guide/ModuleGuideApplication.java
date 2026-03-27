140556cf0c47bde03f4e6149d35122935a2bc556
changhyunmoon <67505983+changhyunmoon@users.noreply.github.com>
2026-03-28 00:24:37 +0900

    test

---
+++ module-guide/src/main/java/com/team6/module/guide/ModuleGuideApplication.java
@@ -0,0 +1,13 @@
+package com.team6.module.guide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleGuideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuleGuideApplication.class, args);
+    }
+
+}
---
+++ module-guide/src/test/java/com/team6/module/guide/ModuleGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team6.module.guide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleGuideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
